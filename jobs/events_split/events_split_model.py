43724eeba10c1791578751266e7226cbe2fc0003
mug <exallion.long@gmail.com>
2022-10-29 12:22:08 +0200

    Some code simplification

--- jobs/events_split/events_split_model.py
+++ jobs/events_split/events_split_model.py
@@ -13,12 +13,12 @@ def split_user_registration_df(data: DataFrame) -> DataFrame:
     :param data: input dataframe.
     :return user_registration dataframe
     """
-    user_registration_df = data.select(
-        "event", "timestamp", "initiator_id", "channel"
-    ).where(data.event == "registered")
-    user_registration_df = user_registration_df.withColumn(
-        "derived_tstamp_day", date_format(col("timestamp"), "yyyy-MM-dd")
-    ).withColumnRenamed("timestamp", "time")
+    user_registration_df = (
+        data.select("event", "timestamp", "initiator_id", "channel")
+        .where(data.event == "registered")
+        .withColumn("derived_tstamp_day", date_format(col("timestamp"), "yyyy-MM-dd"))
+        .withColumnRenamed("timestamp", "time")
+    )
     return user_registration_df
 
 
@@ -30,11 +30,11 @@ def split_app_loaded_df(data: DataFrame) -> DataFrame:
     :param data: input dataframe.
     :return app_loaded dataframe
     """
-    app_loaded_df = data.select(
-        "event", "timestamp", "initiator_id", "device_type"
-    ).where(data.event == "app_loaded")
+    app_loaded_df = (
+        data.select("event", "timestamp", "initiator_id", "device_type")
+        .where(data.event == "app_loaded")
+        .withColumn("derived_tstamp_day", date_format(col("timestamp"), "yyyy-MM-dd"))
+        .withColumnRenamed("timestamp", "time")
+    )
 
-    app_loaded_df = app_loaded_df.withColumn(
-        "derived_tstamp_day", date_format(col("timestamp"), "yyyy-MM-dd")
-    ).withColumnRenamed("timestamp", "time")
     return app_loaded_df
